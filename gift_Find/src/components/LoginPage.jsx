75d100c6d995dc9954455bd9597083569fdb35e4
AdnanYeasir <adnanyeasir6@gmail.com>
2023-11-08 21:02:00 -0500

    testing

---
+++ gift_Find/src/components/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from './Logo';
+import LoginForm from './LoginForm';
+
+const LoginPage = () => {
+  return (
+    <div className="login-page">
+      <Logo />
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
+//testing branch
