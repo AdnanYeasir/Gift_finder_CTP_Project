4302aa048125b5095aaffb483aa13515e12872f1
Stefani Hermanto <sc.shher@gmail.com>
2023-12-13 13:25:05 -0500

    Added gift icon and fade animation on products

--- gift_Find/src/components/Navbar.jsx
+++ gift_Find/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../styles/Navbar.css';
+import GiftLogo from '../assets/gift.png';
 
 function Navbar() {
     return (
         <nav className='navbar'>
-            <div className='navbar-brand'>Gift Finder</div>
+            <div className='navbar-brand'>Gift Finder
+                <img src={GiftLogo} alt='gift logo' className='logo'/>
+            </div>
             <div className='navbar-links'>
                 <a href='/' className='navbar-item'>Home</a>
                 <a href='/favorites' className='navbar-item'>Favorites</a>
